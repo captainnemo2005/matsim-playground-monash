278949fed4e62005b19a6f483d0a82c651565469
captainnemo2005 <kytringuyen95@gmail.com>
2020-09-16 20:31:40 +1000

    first commit

---
+++ src/main/java/Config/TriConfigGroup.java
@@ -0,0 +1,12 @@
+package Config;
+
+import org.matsim.core.config.ReflectiveConfigGroup;
+
+public class TriConfigGroup extends ReflectiveConfigGroup {
+
+
+    public TriConfigGroup(String name) {
+        super(name);
+    }
+    
+}
